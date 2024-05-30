7ebc07feff77bc4e494e42f0fd7d455a527aa0d0
taofeeklanre <lanretaofiq4@gmail.com>
2024-05-30 12:01:58 +0100

    End-User Frontend React Native-3

---
+++ screensHomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <view>
+      </view></button><button title="Fund Wallet" onpress="{()" ==""> navigation.navigate('Wallet')} /?>
+      </button><button title="Select Route" onpress="{()" ==""> navigation.navigate('RouteSelection')} /?>
+    
+  );
+}
